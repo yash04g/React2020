0ec2be78669f9b087d8420a61c79f177a89ab2d3
Yash Gupta <yash04g@gmail.com>
2020-06-12 04:46:17 +0530

    Functional Components

--- starter/src/index.js
+++ starter/src/index.js
@@ -1,15 +1,48 @@
 // Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from './CommentDetail'
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 // Create a react component
+const comment1 = "Nice Blog Post Man!";
+const comment2 = "MotherFucker!";
+const comment3 = "Doesn't make any sense Man!";
+
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author = "Sam" />
-      <CommentDetail author = "Alex" />
-      <CommentDetail author = "Jane" />
+      <ApprovalCard>
+        <div>
+          <h4>Warning</h4>
+          "Are You sure you wanna do this?"
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 5:05 P.M"
+          comment={comment1}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45 A.M"
+          comment={comment2}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 6:45 P.M"
+          comment={comment3}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
