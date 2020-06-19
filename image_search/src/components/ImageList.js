89c1f2ccdcca181853c23b44136b75205c3155cd
Yash Gupta <yash04g@gmail.com>
2020-06-19 19:34:24 +0530

    updated

--- image_search/src/components/ImageList.js
+++ image_search/src/components/ImageList.js
@@ -1,10 +1,11 @@
+import './ImageList.css'
 import React from "react";
 
 const ImageList = (props) => {
   const images = props.images.map(({ id, urls, description }) => {
     return <img key={id} src={urls.regular} alt={description} />;
   });
-  return <div>{images}</div>;
+  return <div className = "image-list">{images}</div>;
 };
 
 export default ImageList;
